2483e393af0fe86dc66c188bf2dd079017605b1d
MIDUNRAJL2000 <midun.raj@edstem.com>
2023-12-26 01:04:50 +0530

    complete

--- src/components/BubbleSort.js
+++ src/components/BubbleSort.js
@@ -1,13 +1,82 @@
+// // import React, { useState } from "react";
+
+// // function BubbleSort() {
+// //   const [inputArray, setInputArray] = useState([]);
+// //   const [sortOrder, setSortOrder] = useState("ascending");
+// //   const [sortedArray, setSortedArray] = useState([]);
+
+// //   const bubbleSort = (arr, order) => {
+// //     const n = arr.length;
+// //     let tempArray = [...arr];
+
+// //     for (let i = 0; i < n - 1; i++) {
+// //       for (let j = 0; j < n - 1 - i; j++) {
+// //         const shouldSwap =
+// //           order === "ascending"
+// //             ? tempArray[j] > tempArray[j + 1]
+// //             : tempArray[j] < tempArray[j + 1];
+// //         if (shouldSwap) {
+// //           [tempArray[j], tempArray[j + 1]] = [tempArray[j + 1], tempArray[j]];
+// //         }
+// //       }
+// //     }
+// //     setSortedArray(tempArray);
+// //   };
+
+// //   const onSortClick = () => {
+// //     bubbleSort(inputArray, sortOrder);
+// //   };
+
+// //   return (
+// //     <div className="container">
+// //       <h1>Bubble sort</h1>
+// //       <div className="input-container">
+// //         <label>
+// //           Enter the array
+// //           <input
+// //             type="text"
+// //             onChange={(e) => {
+// //               const newArray = e.target.value.split(",").map(Number);
+// //               setInputArray(newArray);
+// //             }}
+// //           />
+// //         </label>
+// //         <label>
+// //           Can u please sort the order
+// //           <select onChange={(e) => setSortOrder(e.target.value)}>
+// //             <option value="ascending">Ascending</option>
+// //             <option value="descending">Descending</option>
+// //           </select>
+// //         </label>
+
+// //         <button onClick={onSortClick}>Sort the array</button>
+// //       </div>
+
+// //       <div className="result">
+// //         <h2>Sorted Array:</h2>
+// //         <p>{sortedArray.join(", ")}</p>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// // export default BubbleSort;
+
 import React, { useState } from "react";
 
 function BubbleSort() {
   const [inputArray, setInputArray] = useState([]);
   const [sortOrder, setSortOrder] = useState("ascending");
   const [sortedArray, setSortedArray] = useState([]);
+  const [visualSteps, setVisualSteps] = useState([]);
+  const [currentStep, setCurrentStep] = useState(0);
 
-  const bubbleSort = (arr, order) => {
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+  const bubbleSort = async (arr, order) => {
     const n = arr.length;
     let tempArray = [...arr];
+    let steps = [];
 
     for (let i = 0; i < n - 1; i++) {
       for (let j = 0; j < n - 1 - i; j++) {
@@ -17,13 +86,25 @@ function BubbleSort() {
             : tempArray[j] < tempArray[j + 1];
         if (shouldSwap) {
           [tempArray[j], tempArray[j + 1]] = [tempArray[j + 1], tempArray[j]];
+          steps.push([...tempArray]);
         }
       }
     }
+
+    setVisualSteps(steps);
+    await animateSorting(steps);
     setSortedArray(tempArray);
   };
 
+  const animateSorting = async (steps) => {
+    for (let i = 0; i < steps.length; i++) {
+      setCurrentStep(i);
+      await delay(500); // Adjust the delay time as needed
+    }
+  };
+
   const onSortClick = () => {
+    setCurrentStep(0);
     bubbleSort(inputArray, sortOrder);
   };
 
@@ -42,7 +123,7 @@ function BubbleSort() {
           />
         </label>
         <label>
-          Can u please sort the order
+          Can you please sort the order
           <select onChange={(e) => setSortOrder(e.target.value)}>
             <option value="ascending">Ascending</option>
             <option value="descending">Descending</option>
@@ -54,7 +135,17 @@ function BubbleSort() {
 
       <div className="result">
         <h2>Sorted Array:</h2>
-        <p>{sortedArray.join(", ")}</p>
+        <div className="visualization">
+          {visualSteps.length > 0 &&
+            visualSteps[currentStep]?.map((num, index) => (
+              <div
+                key={index}
+                className={`array-box ${index === currentStep ? "active" : ""}`}
+              >
+                {num}
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
